91b448cdc1208aba5ac62d05b6f8583a2470e4a5
abdelmaksoudhassan <62275775+abdelmaksoudhassan@users.noreply.github.com>
2023-11-14 19:48:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -13,9 +13,9 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Token');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
