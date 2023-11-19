d6ee93f4fe1d0cea0e898dd41320208123be47e1
abdelmaksoudhassan <62275775+abdelmaksoudhassan@users.noreply.github.com>
2023-11-19 05:58:53 +0200

    Update extra-data.model.js

--- database/models/extra-data.model.js
+++ database/models/extra-data.model.js
@@ -9,20 +9,6 @@ ExtraData.init({
         allowNull: false,
         primaryKey: true
     },
-    specialty:{
-        type:Sequelize.DataTypes.STRING,
-        allowNull: false,
-        len: {
-            args: [3,10],
-            msg: 'this field must be 3-10 letters'
-        },
-        notNull:{
-            msg: 'specialty is required'
-        },
-        notEmpty:{
-            msg: 'specialty can\t be empty'
-        },
-    },
     cv:{
         type:Sequelize.DataTypes.STRING
     }
